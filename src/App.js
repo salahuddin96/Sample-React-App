a6326854cf291f0b48b912fb7d64923b7a9bb48e
Salahuddin Shaikh <58594822+salahuddin96@users.noreply.github.com>
2022-10-28 20:38:14 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Uploaded by <b>Salahuddin Bashiruddin Shaikh</b>
         </p>
         <a
           className="App-link"
