5ca5b7c5f3b2ae67c896eb454382ff8d6b508513
Salahuddin Shaikh <58594822+salahuddin96@users.noreply.github.com>
2022-10-28 23:24:40 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Uploaded by <b>Salahuddin Shaikh</b>
+          React App deployment on Azure Storage through GitHub Actions
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          SALAHUDDIN
         </a>
       </header>
     </div>
